c3adc5d12fe19d277c9c6335a221a34b5c80b71f
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-09-14 23:47:13 +0600

    getting input from user

--- Ex-One.c
+++ Ex-One.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main(){
-    int a;
+    int a, b;
+    b = 10;
     printf("Enter the value of A: ");
-    scanf("%d", &a);
-    printf("The value of A is: %d", a);
-    return 0;
+    scanf("%d", &a); // Taking Input From User To Multiply With (b).
+    printf("The value of A is: %d", b*a); // Here I'm Calculating.
+    return 0; // I Don't Get This (return 0;)
 }
