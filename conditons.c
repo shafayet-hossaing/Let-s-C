232ec79f4330f3cb26515626bc0b63fe68547bb7
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-09-15 22:24:41 +0600

    added more commnet

--- conditons.c
+++ conditons.c
@@ -5,7 +5,7 @@ int main(){
     // Voting system with condition
     int age;
     scanf("%d", &age);
-    printf("Your age is : %d\n", age);
+    printf("Your age is : %d\n", age); // By this line i'm confirming the number user inserting. 
     if(age>18){
         printf("You can vote");
     }else{
