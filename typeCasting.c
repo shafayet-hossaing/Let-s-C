8bb023a2d3bcdf159b0971eaacb83f3f830bde8c
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-09-20 02:13:37 +0600

    typecasting

---
+++ typeCasting.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int b = 52;
+    float a = 54;
+    printf("The value of a is : %d", (int) a); // This is how we can conver the type of values.
+    printf("The value of b is : %f", (float) b);
+    return 0;
+}
